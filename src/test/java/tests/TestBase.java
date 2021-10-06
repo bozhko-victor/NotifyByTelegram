bb6eb8a850f53588bd8cea27c751cbf205eae25d
bozhko-victor <bozhko.v.n@yandex.ru>
2021-10-06 16:12:58 +0300

    Added Attach

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,7 +6,7 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
-//import helpers.Attach;
+import helpers.Attach;
 
 
 public class TestBase {
@@ -23,12 +23,12 @@ public class TestBase {
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
     }
 
-//    @AfterEach
-//    public void tearDown() {
-//        Attach.screenshotAs("Last screenshot");
-//        Attach.pageSource();
-//        Attach.browserConsoleLogs();
-//        Attach.addVideo();
-//    }
+    @AfterEach
+    public void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
 
 }
